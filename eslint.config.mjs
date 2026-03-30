17aae74a31d8456e4d70fd5ccbb897336db374a7
Jan Skiba <jan.skiba.poczta@proton.me>
2026-03-30 21:43:56 +0200

    SCRUM-7: Refactor ESLint and Prettier configurations, update project tags, and remove unused config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,43 @@
 import nx from '@nx/eslint-plugin';
+import playwright from 'eslint-plugin-playwright';
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+  ...nx.configs['flat/angular'],
+  ...nx.configs['flat/angular-template'],
+  {
+    ...playwright.configs['flat/recommended'],
+    files: [
+      'apps/frontend-e2e/**/*.ts',
+      'apps/frontend-e2e/**/*.js',
+      'apps/frontend-e2e/playwright.config.ts',
+    ],
+  },
   {
     ignores: ['**/dist', '**/out-tsc', '**/vitest.config.*.timestamp*'],
+  },
+  {
+    files: ['apps/frontend/**/*.ts'],
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
@@ -17,8 +49,28 @@ export default [
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: 'type:app',
+              onlyDependOnLibsWithTags: ['type:lib'],
+            },
+            {
+              sourceTag: 'type:e2e',
+              onlyDependOnLibsWithTags: ['type:app', 'type:lib'],
+            },
+            {
+              sourceTag: 'type:lib',
+              onlyDependOnLibsWithTags: ['type:lib'],
+            },
+            {
+              sourceTag: 'scope:backend',
+              onlyDependOnLibsWithTags: ['scope:backend', 'scope:shared'],
+            },
+            {
+              sourceTag: 'scope:frontend',
+              onlyDependOnLibsWithTags: ['scope:frontend', 'scope:shared'],
+            },
+            {
+              sourceTag: 'scope:shared',
+              onlyDependOnLibsWithTags: ['scope:shared'],
             },
           ],
         },
