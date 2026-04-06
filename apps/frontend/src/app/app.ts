08952539d6ebcb94a2b38b90136969a497d35c7e
Jan Skiba <jan.skiba.poczta@proton.me>
2026-04-06 20:19:16 +0200

    SCRUM-59: feat: restructure app layout to include Sidenav and Header components

--- apps/frontend/src/app/app.ts
+++ apps/frontend/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './shared/ui/header.component';
+import { SidenavComponent } from './shared/ui/sidenav.component';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent, SidenavComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
