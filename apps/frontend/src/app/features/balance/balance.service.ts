a50cf21cd10d7aeb585df6430334884b68284f10
janskiba <jan.skiba.poczta@proton.me>
2026-04-14 23:13:52 +0200

    SCRUM-63: feat(balance): implement balance component with NgRx store integration and loading state handling

--- apps/frontend/src/app/features/balance/balance.service.ts
+++ apps/frontend/src/app/features/balance/balance.service.ts
@@ -1,13 +1,17 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Balance } from '@packages/types';
 
 @Injectable({ providedIn: 'root' })
 export class BalanceService {
   private readonly http = inject(HttpClient);
 
+  // To implement
   getBalance(): Observable<Balance> {
-    return this.http.get<Balance>('/api/balance');
+    return of({  income: 18750.8,
+  expenses: 6300,
+  balance: 12450.8})
+    // return this.http.get<Balance>('/api/balance');
   }
 }
--- apps/frontend/src/app/features/balance/components/balance.component.spec.ts
+++ apps/frontend/src/app/features/balance/components/balance.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 import { BalanceComponent } from './balance.component';
 
 describe('BalanceComponent', () => {
@@ -8,6 +9,17 @@ describe('BalanceComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BalanceComponent],
+      providers: [
+        provideMockStore({
+          initialState: {
+            balance: {
+              data: null,
+              loading: false,
+              error: null,
+            },
+          },
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BalanceComponent);
