ba7428328ae1e88274ac783882de15baa42fc7b1
janskiba <jan.skiba.poczta@proton.me>
2026-04-17 23:10:50 +0200

    SCRUM-63: feat(dashboard): update DashboardComponent template and remove unused styles

--- apps/frontend/src/app/pages/dashboard.component.ts
+++ apps/frontend/src/app/pages/dashboard.component.ts
@@ -1,25 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BalanceComponent } from '../features/balance/components/balance.component';
-import { BalanceStatComponent } from "../features/balance/components/balance-stat.component";
+import { BalanceStatComponent } from '../features/balance/components/balance-stat.component';
 
 @Component({
   selector: 'app-dashboard',
   imports: [BalanceComponent, BalanceStatComponent],
-  template: `<section class="dashboard-layout">
+  template: `<section class="grid grid-cols-1 gap-6 lg:grid-cols-[1fr_0.5fr_0.5fr]">
     <app-balance />
     <app-balance-stat type="income" />
     <app-balance-stat type="expenses" />
   </section>`,
-  styles: `
-    :host {
-      display: block;
-    }
-
-    .dashboard-layout {
-      display: grid;
-      gap: 1.5rem;
-    }
-  `,
+  host: {
+    class: 'block',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {}
